89447284904be2d7737d53f18c3ff5718a2458a1
Steffany <kielen@gmail.com>
2018-07-09 12:49:37 -0700

    completed single item view

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,13 @@ import List from './list';
 import 'materialize-css/dist/css/materialize.min.css';
 import AddItem from '../components/add_item';
 import {Route} from 'react-router-dom';
+import ViewItem from './view_item';
 
 const App = () => (
     <div className='container'>
         <Route exact path='/' component={List} />
         <Route path='/add-item' component={AddItem} />
+        <Route path='/todo/:item_id' component={ViewItem} />
     </div>
 );
 
